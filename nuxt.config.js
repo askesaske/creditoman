55b530d361b412f1864d13c9507f1e427167883d
Orazalinov Askhat <58249226+askesaske@users.noreply.github.com>
2021-03-05 21:24:59 +0600

    link preview meta tags added

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,9 @@ export default {
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
+      {hid: 'description', name: 'description', content: ''},
+      {name: 'og:title', content: 'CashSoSwiftly || Payday Lending'},
+      {name: 'og:description', content: ''},
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
